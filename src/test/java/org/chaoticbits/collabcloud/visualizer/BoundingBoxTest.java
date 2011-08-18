650efe9c7ed8846c7ced5a6d64c0b7f0e281917e
andy.meneely <andy.meneely@gmail.com>
2011-08-18 13:02:21 -0400

    don't really need my own class - just use Rectangle2D

--- src/test/java/org/chaoticbits/collabcloud/visualizer/BoundingBoxTest.java
+++ src/test/java/org/chaoticbits/collabcloud/visualizer/BoundingBoxTest.java
@@ -3,52 +3,60 @@ package org.chaoticbits.collabcloud.visualizer;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.awt.geom.Rectangle2D;
+
+/**
+ * Wrote this test because I don't trust the Rectangle2D intersect method.
+ * 
+ * @author andy
+ * 
+ */
 public class BoundingBoxTest {
 
 	@org.junit.Test
 	public void notIntersectingBothAxes() throws Exception {
-		BoundingBox a = new BoundingBox(new PointXY(0.0d, 0.0d), new PointXY(1.0d, 1.0d));
-		BoundingBox b = new BoundingBox(new PointXY(1.5d, 1.5d), new PointXY(2.0d, 2.0d));
+		Rectangle2D a = new Rectangle2D.Double(0.0d, 0.0d, 1.0d, 1.0d);
+		Rectangle2D b = new Rectangle2D.Double(1.5d, 1.5d, 2.0d, 2.0d);
 		assertFalse("boxes do not intersect each other", a.intersects(b));
 		assertFalse("reverse is true too", b.intersects(a));
 	}
 
 	@org.junit.Test
 	public void notIntersectingXAxes() throws Exception {
-		BoundingBox a = new BoundingBox(new PointXY(0.0d, 1.0d), new PointXY(1.0d, 2.0d));
-		BoundingBox b = new BoundingBox(new PointXY(1.5d, 1.5d), new PointXY(2.0d, 2.0d));
+		Rectangle2D a = new Rectangle2D.Double(0.0d, 1.0d, 1.0d, 2.0d);
+		Rectangle2D b = new Rectangle2D.Double(1.5d, 1.5d, 2.0d, 2.0d);
 		assertFalse("boxes do not intersect each other", a.intersects(b));
 		assertFalse("reverse is true too", b.intersects(a));
 	}
 
 	@org.junit.Test
 	public void notIntersectingYAxes() throws Exception {
-		BoundingBox a = new BoundingBox(new PointXY(1.0d, 0.0d), new PointXY(2.0d, 1.0d));
-		BoundingBox b = new BoundingBox(new PointXY(1.5d, 1.5d), new PointXY(2.0d, 2.0d));
+		Rectangle2D a = new Rectangle2D.Double(1.0d, 0.0d, 2.0d, 1.0d);
+		Rectangle2D b = new Rectangle2D.Double(1.5d, 1.5d, 2.0d, 2.0d);
 		assertFalse("boxes do not intersect each other", a.intersects(b));
 		assertFalse("reverse is true too", b.intersects(a));
 	}
 
 	@org.junit.Test
 	public void doIntersectSameAxes() throws Exception {
-		BoundingBox a = new BoundingBox(new PointXY(0.0d, 0.0d), new PointXY(2.0d, 2.0d));
-		BoundingBox b = new BoundingBox(new PointXY(1.0d, 1.0d), new PointXY(3.0d, 3.0d));
+		Rectangle2D a = new Rectangle2D.Double(0.0d, 0.0d, 2.0d, 2.0d);
+		Rectangle2D b = new Rectangle2D.Double(1.0d, 1.0d, 3.0d, 3.0d);
 		assertTrue("boxes do intersect each other", a.intersects(b));
 		assertTrue("reverse is true too", b.intersects(a));
 	}
 
 	@org.junit.Test
 	public void doIntersectJustVertical() throws Exception {
-		BoundingBox a = new BoundingBox(new PointXY(0.0d, 0.0d), new PointXY(2.0d, 2.0d));
-		BoundingBox b = new BoundingBox(new PointXY(0.0d, 1.0d), new PointXY(2.0d, 3.0d));
+		Rectangle2D a = new Rectangle2D.Double(0.0d, 0.0d, 2.0d, 2.0d);
+		Rectangle2D b = new Rectangle2D.Double(0.0d, 1.0d, 2.0d, 3.0d);
 		assertTrue("boxes do intersect each other", a.intersects(b));
 		assertTrue("reverse is true too", b.intersects(a));
 	}
 
 	@org.junit.Test
 	public void doIntersectJustHorizontal() throws Exception {
-		BoundingBox a = new BoundingBox(new PointXY(0.0d, 0.0d), new PointXY(2.0d, 2.0d));
-		BoundingBox b = new BoundingBox(new PointXY(1.0d, 0.0d), new PointXY(3.0d, 2.0d));
+		Rectangle2D a = new Rectangle2D.Double(0.0d, 0.0d, 2.0d, 2.0d);
+		Rectangle2D b = new Rectangle2D.Double(1.0d, 0.0d, 3.0d, 2.0d);
 		assertTrue("boxes do intersect each other", a.intersects(b));
 		assertTrue("reverse is true too", b.intersects(a));
 	}
