17fa28c91f11b89753b136cbee531967439b1642
andy.meneely <andy.meneely@gmail.com>
2011-08-19 11:48:55 -0400

    added a fun thing to summarize my current code base

---
+++ src/main/java/org/chaoticbits/collabcloud/codeprocessor/SummarizeThis.java
@@ -0,0 +1,32 @@
+package org.chaoticbits.collabcloud.codeprocessor;
+
+import japa.parser.JavaParser;
+import japa.parser.ParseException;
+import japa.parser.ast.CompilationUnit;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
+public class SummarizeThis {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 * @throws ParseException
+	 */
+	public static void main(String[] args) throws ParseException, IOException {
+		File[] files = new File("src/main/java/org/chaoticbits/collabcloud/codeprocessor").listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".java");
+			}
+		});
+		CloudWeights weights = new CloudWeights();
+		for (File file : files) {
+			CompilationUnit unit = JavaParser.parse(file);
+			weights = unit.accept(new Summarizer(), weights);
+		}
+		System.out.println("==Weights of this source code==");
+		System.out.println(weights);
+	}
+}
