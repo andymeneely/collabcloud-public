e859795d54b8f0c2fd0c5bf8ca834946a5e6bad3
Andy Meneely <andy.meneely@gmail.com>
2011-08-30 09:44:42 -0400

    updated summarize repo to use the new api

--- src/test/java/org/chaoticbits/collabcloud/SummarizeRepo.java
+++ src/test/java/org/chaoticbits/collabcloud/SummarizeRepo.java
@@ -8,11 +8,12 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.Shape;
 import java.awt.font.FontRenderContext;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Random;
@@ -27,24 +28,29 @@ import org.chaoticbits.collabcloud.codeprocessor.MultiplyModifier;
 import org.chaoticbits.collabcloud.codeprocessor.java.JavaProjectSummarizer;
 import org.chaoticbits.collabcloud.vc.git.GitLoader;
 import org.chaoticbits.collabcloud.vc.git.GitLoaderTest;
+import org.chaoticbits.collabcloud.visualizer.IPlaceStrategy;
 import org.chaoticbits.collabcloud.visualizer.Intersector;
 import org.chaoticbits.collabcloud.visualizer.LastHitCache;
 import org.chaoticbits.collabcloud.visualizer.LastHitCache.IHitCheck;
+import org.chaoticbits.collabcloud.visualizer.RandomPlacement;
 import org.chaoticbits.collabcloud.visualizer.SpiralIterator;
+import org.chaoticbits.collabcloud.visualizer.font.BoundedLogFont;
+import org.chaoticbits.collabcloud.visualizer.font.IFontTransformer;
 
 public class SummarizeRepo {
-	private static final int STARTING_POINT_JITTER = 10;
 	private static final double LEAF_CUTOFF = 1.0d;
 	private static final int SPIRAL_STEPS = 500;
 	private static final double SPIRAL_MAX_RADIUS = 400.0d;
-	private static final double SQUASHDOWN = 2.5;
+	private static final double SQUASHDOWN = 2;
 	private static final File TEST_BED = new File("testgitrepo");
 	// private static final File THIS_REPO = new File("");
 	// private static final String THIS_REPO_SECOND_COMMIT_ID = "4cfde077a84185b06117bcff5d47c53644463b1f";
-	private static final Random rand = new Random(1234567L);
+	private static final Random RAND = new Random(123456789L);
+	private static final IPlaceStrategy PLACE_STRATEGY = new RandomPlacement(RAND, new Rectangle2D.Double(300, 400, 50, 50)) ;
 
 	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(SummarizeRepo.class);
 	private static IWeightModifier modifier = new MultiplyModifier(1.1);
+	private static Font INITIAL_FONT = new Font("Courier New", Font.BOLD, 150);
 
 	private static final Intersector intersector = new Intersector(10, LEAF_CUTOFF);
 	private static final IHitCheck<Shape> checker = new IHitCheck<Shape>() {
@@ -79,34 +85,27 @@ public class SummarizeRepo {
 		g2d.fillRect(0, 0, 800, 800);
 		g2d.setColor(Color.DARK_GRAY);
 		g2d.drawRect(0, 0, WIDTH - 1, HEIGHT - 1);
-		AffineTransform transform = g2d.getTransform();
-		Font font = new Font("Courier New", Font.BOLD, 150);
+
 		FontRenderContext frc = new FontRenderContext(null, true, true);
 		LastHitCache<Shape> placedShapes = new LastHitCache<Shape>(checker);
 		List<Entry<ISummaryToken, Double>> entries = weights.sortedEntries();
-		float fontMultiplier = 75f / (float) Math.log(entries.get(0).getValue());
-		// Collections.shuffle(entries, rand);
+		IFontTransformer fontTransformer = new BoundedLogFont(INITIAL_FONT, weights, 75d);
+		Collections.shuffle(entries, RAND);
 		for (Entry<ISummaryToken, Double> entry : entries) {
-			// TODO Convert weights to font sizes
-			float fontSize = fontMultiplier * (float) Math.log(entry.getValue());
-			// float fontSize = fontMultiplier * (float) Math.sqrt(entry.getValue());
-			if (fontSize < 6f)
+			Font font = fontTransformer.transform(entry.getValue());
+			if (font.getSize2D() < 6f)
 				continue;
 			log.info("Laying out " + entry.getKey() + "...[" + entry.getValue() + "]");
-			font = font.deriveFont(fontSize);
 
-			// font = font.deriveFont(20f * (float) Math.sqrt(entry.getValue()));
 			char[] chars = entry.getKey().getToken().toCharArray();
-			// TODO Need a placement strategy
-			Point2D center = getStartingPlace();
+			Point2D center = PLACE_STRATEGY.getStartingPlace(entry.getKey());
 			SpiralIterator spiral = new SpiralIterator(center, SPIRAL_MAX_RADIUS, SPIRAL_STEPS, SQUASHDOWN);
 			while (spiral.hasNext()) {
 				Point2D next = spiral.next();
 				Shape nextShape = font.layoutGlyphVector(frc, chars, 0, chars.length, Font.LAYOUT_LEFT_TO_RIGHT).getOutline((float) next.getX(),
 						(float) next.getY());
 				if (!placedShapes.hitNCache(nextShape)) {
-					int randColor = (int) (rand.nextFloat() * 150.0f + 25.0f);
-					g2d.setTransform(transform);
+					int randColor = (int) (RAND.nextFloat() * 150.0f + 25.0f);
 					// TODO Color it according to type and weight, not random
 					g2d.setColor(new Color(randColor, randColor, randColor));
 					g2d.fill(nextShape);
@@ -122,10 +121,5 @@ public class SummarizeRepo {
 		log.info("Writing image...");
 		ImageIO.write(bi, "PNG", new File("output/summarizerepo.png"));
 		log.info("Done!");
-	}
-
-	private static java.awt.geom.Point2D.Double getStartingPlace() {
-		// return new Point2D.Double(rand.nextInt(300) + 150f, rand.nextInt(300) + 150.0f);
-		return new Point2D.Double(300 + rand.nextDouble() * STARTING_POINT_JITTER, 400 + rand.nextDouble() * STARTING_POINT_JITTER);
 	}
 }
