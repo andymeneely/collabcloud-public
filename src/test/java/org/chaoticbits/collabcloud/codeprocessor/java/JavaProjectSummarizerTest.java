e49790a31514e546470892d01f80ca9e32347701
Andy Meneely <andy.meneely@gmail.com>
2011-08-30 16:46:39 -0400

    can't get stupid powermock to work yet...

--- src/test/java/org/chaoticbits/collabcloud/codeprocessor/java/JavaProjectSummarizerTest.java
+++ src/test/java/org/chaoticbits/collabcloud/codeprocessor/java/JavaProjectSummarizerTest.java
@@ -1,7 +1,7 @@
 package org.chaoticbits.collabcloud.codeprocessor.java;
 
 import static org.easymock.EasyMock.expect;
-import static org.powermock.api.easymock.PowerMock.mockStatic;
+import static org.powermock.api.easymock.PowerMock.*;
 import japa.parser.JavaParser;
 import japa.parser.ast.CompilationUnit;
 
@@ -9,6 +9,7 @@ import java.io.File;
 
 import org.chaoticbits.collabcloud.codeprocessor.CloudWeights;
 import org.easymock.EasyMock;
+import org.easymock.IMocksControl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.easymock.PowerMock;
@@ -21,23 +22,26 @@ public class JavaProjectSummarizerTest {
 
 	@Test
 	public void testProject() throws Exception {
+		IMocksControl ctrl = EasyMock.createStrictControl();
 		JavaProjectSummarizer summarizer = new JavaProjectSummarizer();
-		mockStatic(JavaParser.class);
+		PowerMock.mockStatic(JavaParser.class);
 		CompilationUnit unit = PowerMock.createMock(CompilationUnit.class);
-		File mockDir = PowerMock.createMock(File.class);
-		File mockFile = PowerMock.createMock(File.class);
+		File mockDir = ctrl.createMock(File.class);
+		File mockFile = ctrl.createMock(File.class);
 		CloudWeights weights = new CloudWeights();
 
 		expect(mockDir.isDirectory()).andReturn(true).anyTimes();
 		expect(mockDir.listFiles()).andReturn(new File[] { mockFile }).once();
 		expect(mockFile.isDirectory()).andReturn(false).anyTimes();
 		expect(mockFile.getName()).andReturn("a.java").anyTimes();
-		expect(JavaParser.parse(mockFile)).andReturn(unit);
-		expect(unit.accept((JavaSummarizeVisitor) EasyMock.anyObject(), (CloudWeights) EasyMock.anyObject())).andReturn(weights).once();
+		expect(JavaParser.parse(mockFile)).andReturn(unit).once();
+//		expect(unit.accept((JavaSummarizeVisitor) EasyMock.anyObject(), (CloudWeights) EasyMock.anyObject())).andReturn(weights).once();
+		ctrl.replay();
 		PowerMock.replay();
 
 		summarizer.summarize(mockDir);
 
-		EasyMock.verify();
+		PowerMock.verify(JavaParser.class);
+		ctrl.verify();
 	}
 }
