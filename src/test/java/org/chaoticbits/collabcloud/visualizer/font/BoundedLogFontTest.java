e0e085eb9bda4c7b18bb6bc5596300601ac2ac57
Andy Meneely <andy.meneely@gmail.com>
2011-09-20 15:15:02 -0400

    updated for the latest bounded log font

--- src/test/java/org/chaoticbits/collabcloud/visualizer/font/BoundedLogFontTest.java
+++ src/test/java/org/chaoticbits/collabcloud/visualizer/font/BoundedLogFontTest.java
@@ -6,6 +6,8 @@ import java.awt.Font;
 
 import org.chaoticbits.collabcloud.CloudWeights;
 import org.chaoticbits.collabcloud.ISummaryToken;
+import org.chaoticbits.collabcloud.codeprocessor.java.JavaSummaryToken;
+import org.chaoticbits.collabcloud.codeprocessor.java.JavaTokenType;
 import org.easymock.EasyMock;
 import org.easymock.IMocksControl;
 import org.junit.Before;
@@ -29,9 +31,9 @@ public class BoundedLogFontTest {
 		weights.put(ctrl.createMock(ISummaryToken.class), 10d);
 		IFontTransformer trans = new BoundedLogFont(font, weights, 75);
 		ctrl.replay();
-		assertEquals(75d, trans.transform(null, 1000d).getSize2D(), 0.001);
-		assertEquals(67.474d, trans.transform(null, 500d).getSize2D(), 0.001);
-		assertEquals(0d, trans.transform(null, 1d).getSize2D(), 0.001);
+		assertEquals(75.0108d, trans.transform(token("something"), 1000d).getSize2D(), 0.001);
+		assertEquals(67.495d, trans.transform(token("something"), 500d).getSize2D(), 0.001);
+		assertEquals(7.525d, trans.transform(token("something"), 1d).getSize2D(), 0.001);
 		ctrl.verify();
 	}
 
@@ -43,7 +45,11 @@ public class BoundedLogFontTest {
 		weights.put(ctrl.createMock(ISummaryToken.class), -10d);
 		IFontTransformer trans = new BoundedLogFont(font, weights, 75);
 		ctrl.replay();
-		assertEquals(74.89d, trans.transform(null, 1000d).getSize2D(), 0.001);
+		assertEquals(75.0108d, trans.transform(token("something"), 1000d).getSize2D(), 0.001);
 		ctrl.verify();
+	}
+
+	private ISummaryToken token(String string) {
+	return new JavaSummaryToken(null, string, string, JavaTokenType.CLASS);
 	}
 }
