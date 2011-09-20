bb463d254624f74a5c547d3ccfbe4ae6484abd8b
Andy Meneely <andy.meneely@gmail.com>
2011-09-20 16:57:41 -0400

    builds the token contribution network
    This needs to be changed at some point.

--- src/main/java/org/chaoticbits/collabcloud/vc/TokenContributionNetwork.java
+++ src/main/java/org/chaoticbits/collabcloud/vc/TokenContributionNetwork.java
@@ -1,12 +1,14 @@
 package org.chaoticbits.collabcloud.vc;
 
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.chaoticbits.collabcloud.ISummarizable;
 import org.chaoticbits.collabcloud.ISummaryToken;
+import org.chaoticbits.collabcloud.codeprocessor.java.JavaSummaryToken;
+import org.chaoticbits.collabcloud.codeprocessor.java.JavaTokenType;
 import org.chaoticbits.collabcloud.visualizer.placement.INetworkBuilder;
-import org.eclipse.jgit.revwalk.RevCommit;
 
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.UndirectedSparseGraph;
@@ -21,53 +23,16 @@ public class TokenContributionNetwork implements INetworkBuilder {
 
 	public Graph<ISummaryToken, Long> build() {
 		Graph<ISummaryToken, Long> graph = new UndirectedSparseGraph<ISummaryToken, Long>();
-		throw new IllegalStateException("unimplemented!");
-		// try {
-		// RevCommit next = itr.next();
-		// RevTree tree = next.getTree();
-		// TreeWalk walk = new TreeWalk(repo);
-		// DiffFormatter formatter = new DiffFormatter(DisabledOutputStream.INSTANCE);
-		// Git git = new Git(repo);
-		// LogCommand log = git.log();
-		//
-		// walk.addTree(tree);
-		// walk.setRecursive(true);
-		// while (walk.next()) {
-		// System.out.println(walk.getPathString());
-		// }
-		//
-		// } catch (AmbiguousObjectException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (IOException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// while (itr.hasNext()) {
-		// RevCommit commit = itr.next();
-		// try {
-		// TreeWalk walk= new TreeWalk(repo);
-		// walk.addTree(commit.getTree());
-		// walk.setRecursive(true);
-		// walk.next();
-		// do{
-		// String pathString = walk.getPathString();
-		// System.out.println(pathString);
-		// }while(walk.next());
-		//
-		// } catch (MissingObjectException e) {
-		// e.printStackTrace();
-		// } catch (IOException e) {
-		// e.printStackTrace();
-		// }
-		// // graph.add(edgeCount++, author(commit), file);
-		// }
+		long edge = 0;
+		for (Entry<Developer, Set<ISummarizable>> entry : contributions.entrySet()) {
+			for (ISummarizable summarizable : entry.getValue()) {
+				//TODO Need to pull out the tokens from the summarizable
+				ISummaryToken token = new JavaSummaryToken(summarizable, summarizable.getFile().getName(), summarizable.getFile().getName(),
+						JavaTokenType.CLASS);
+				graph.addEdge(edge++, entry.getKey(), token);
+			}
+		}
+		return graph;
 
-		// return graph;
-
-	}
-
-	private Developer author(RevCommit commit) {
-		return new Developer(commit.getAuthorIdent().getName(), commit.getAuthorIdent().getEmailAddress());
 	}
 }
