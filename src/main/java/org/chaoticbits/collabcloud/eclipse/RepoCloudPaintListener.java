23e96f98e77e7fa7a79bc780346e2b024e32cfba
Andy Meneely <andy.meneely@gmail.com>
2012-05-22 22:06:07 -0400

    starting the great eclipse migration

---
+++ src/main/java/org/chaoticbits/collabcloud/eclipse/RepoCloudPaintListener.java
@@ -0,0 +1,10 @@
+package org.chaoticbits.collabcloud.eclipse;
+
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+
+public class RepoCloudPaintListener implements PaintListener {
+
+	public void paintControl(PaintEvent arg0) {}
+
+}
