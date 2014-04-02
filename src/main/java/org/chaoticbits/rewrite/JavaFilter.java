b490e0c5358b26748b8fe2eaf44ba4ea6cacabb7
Marissa K Wilson <Marissa@MarissaKWilson.com>
2014-04-02 00:59:32 -0400

    Added 'Filter' and 'Compare' functions

--- src/main/java/org/chaoticbits/rewrite/JavaFilter.java
+++ src/main/java/org/chaoticbits/rewrite/JavaFilter.java
@@ -1,5 +1,32 @@
 package org.chaoticbits.rewrite;
 
-public class JavaFilter {
+import java.util.List;
 
+/**
+ * Filters out words that may be Java keywords
+ * or words that are in the 'excluded' list
+ * @author Marissa Wilson
+ *
+ */
+public class JavaFilter {
+	/*
+	 * Takes in repo files
+	 * Calls Compare on each potential token in turn
+	 * If true, moves on (could potentially cache for quicker reruns)
+	 * If false, creates new token.
+	 * Returns list of tokens
+	 */
+	public static List Filter(repofiles){
+		
+	}
+	/*
+	 * Takes in a potential token (potoken)
+	 * Checks against the excluded words list
+	 * Checks against Java keywords
+	 * Returns true if potoken is found in either
+	 * Returns false if potoken should be made into a token
+	 */
+	public static boolean Compare(String potoken){
+		
+	}
 }
